66626d6b426646cfe418087d10150ee1c3a1df2b
obwoj1 <149099282+obwoj1@users.noreply.github.com>
2024-06-14 10:55:41 -0400

    map

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,7 +9,7 @@ export const About = () => {
       <h2 className={styles.title}>About</h2>
       <div className={styles.content}>
         <img
-          src={getImageUrl("about/aboutImage.png")}
+          src={getImageUrl("about/obeya-modified.png")}
           alt="Me sitting with a laptop"
           className={styles.aboutImage}
         />
@@ -27,21 +27,21 @@ export const About = () => {
           <li className={styles.aboutItem}>
             <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
             <div className={styles.aboutItemText}>
-              <h3>Backend Developer</h3>
+              {/* <h3>Backend Developer</h3> */}
               <p>
-                I have experience developing fast and optimised back-end systems
-                and APIs
+                {/* I have experience developing fast and optimised back-end systems
+                and APIs */}
               </p>
             </div>
           </li>
           <li className={styles.aboutItem}>
             <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
             <div className={styles.aboutItemText}>
-              <h3>UI Designer</h3>
+              {/* <h3>UI Designer</h3>
               <p>
                 I have designed multiple landing pages and have created design
                 systems as well
-              </p>
+              </p> */}
             </div>
           </li>
         </ul>
